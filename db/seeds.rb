595cf4d086b83ac2a33744aa190852c9eeb9dadf
jonmackeyparty <jon@jonmackey.party>
2019-06-20 17:08:49 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+games = Game.create([{state: ['', '', '', '', '', '', '', '', '']}, {state: ['O', 'X', 'O', '', 'X', 'X', '', 'X', 'O']}, {state: ['O', 'X', '', '', '', '', '', '', '']}])
